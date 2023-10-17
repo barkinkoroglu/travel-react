f140d765bd61f4be908ba7026e14c6bd108056e2
Barkin Koroglu <barkin_45_99@hotmail.com>
2023-10-17 14:59:34 +0300

    Destinations

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Destination from './components/Destination';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
@@ -6,6 +7,7 @@ export default function App() {
     <div>
       <Navbar />
       <Hero />
+      <Destination />
     </div>
   );
 }
